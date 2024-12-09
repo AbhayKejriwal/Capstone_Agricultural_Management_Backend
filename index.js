5a4269c772ff906b614e03678f3910cc9e323442
Abhay Kejriwal <121892598+AbhayKejriwal@users.noreply.github.com>
2024-12-09 09:38:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ import marketRoutes from './routes/market.routes.js';
 const app = express();
 
 const corsOptions = {
-    origin: 'https://capstone-agricultural-management-frontend.vercel.app/',
+    origin: 'https://capstone-agricultural-management-frontend.vercel.app',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     optionsSuccessStatus: 200
